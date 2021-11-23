f9a05d83cede7f807124190549e15efd76982bff
AlexPC99 <75812731+AlexPC99@users.noreply.github.com>
2021-11-22 21:47:34 -0600

    Update MacTest.py

--- MacTest.py
+++ MacTest.py
@@ -4,10 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
-from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from fpdf import FPDF
 
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(chrome_options=options, executable_path=r'/Users/mch/Documents/GitHub/Prueba/chromedriver')
+driver = webdriver.Chrome("chromedriver")
 driver.get("https://www.pressreader.com/mexico/el-universal/20211103/page/1/textview")
